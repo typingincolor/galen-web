5919b6d492edf80cb7c62032c6ba697f65780386
Andrew Braithwaite <abraithw@gmail.com>
2015-07-13 19:58:34 +0100

    start the web app

---
+++ src/main/java/info/losd/galenweb/api/GalenWebApiController.java
@@ -0,0 +1,55 @@
+package info.losd.galenweb.api;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2015 Andrew Braithwaite
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+@RestController
+public class GalenWebApiController {
+    InfluxDB influxDB = InfluxDBFactory.connect("http://docker.local:8086", "root", "root");
+
+    @RequestMapping(value = "/apis", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getApiList() {
+        Query query = new Query("SHOW TAG VALUES FROM statistic WITH KEY = api", "galen");
+        QueryResult apiList = influxDB.query(query);
+
+        List<String> apis = new LinkedList<String>();
+        apiList.getResults().get(0).getSeries().get(0).getValues().forEach(value -> {
+           apis.add(value.get(0).toString());
+        });
+
+        return apis;
+    }
+}
